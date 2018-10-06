6dd4588469e97f20b044551db1961249be39be78
JacobDixon0 <jacobdixon001@gmail.com>
2018-10-06 14:57:07 -0400

    Added player with collision.

--- Game/src/MainApplication.java
+++ Game/src/MainApplication.java
@@ -1,9 +1,20 @@
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainApplication extends Application {
+
+    private static boolean up = false;
+    private static boolean right = false;
+    private static boolean left = false;
+
+    public static int winX = 600;
+    public static int winY = 400;
+
+    Player player = new Player(0, winY-100, 100, 100);
 
     public static void main ( String[] args ) {
         launch(args);
@@ -13,9 +24,65 @@ public class MainApplication extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         Pane root = new Pane();
+        Scene scene = new Scene(root, winX, winY);
 
-        primaryStage.setScene(new Scene(root, 300, 300));
+        scene.setOnKeyPressed(e ->{
+            KeyCode key = e.getCode();
+
+            if(key == KeyCode.RIGHT){
+                right = true;
+            }else if(key == KeyCode.LEFT){
+                left = true;
+            }else if(key == KeyCode.UP){
+                up = true;
+            }
+
+        });
+
+        scene.setOnKeyReleased(e ->{
+            KeyCode key = e.getCode();
+
+            if(key == KeyCode.RIGHT){
+                right = false;
+            }else if(key == KeyCode.LEFT){
+                left = false;
+            }else if(key == KeyCode.UP){
+                up = false;
+            }
+
+        });
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                update();
+            }
+        };
+
+        timer.start();
+
+        root.getChildren().add(player);
+
+        primaryStage.setResizable(false);
+        primaryStage.sizeToScene();
+        primaryStage.setTitle("Title");
+        primaryStage.setScene(scene);
         primaryStage.show();
+
+    }
+
+    void update(){
+        if(right){
+            if(!player.isCollidingRight()){
+                player.moveRight();
+            }
+        }else if (left){
+            if(!player.isCollidingLeft()) {
+                player.moveLeft();
+            }
+        }else if(up){
+                player.jump();
+        }
 
     }
 }
--- Game/src/Player.java
+++ Game/src/Player.java
@@ -1,2 +1,61 @@
-public class Player {
+import javafx.scene.shape.Rectangle;
+
+public class Player extends Rectangle {
+
+    double vX = 10.0;
+    double vY = 10.0;
+    double g = 4;
+
+    Player(double x, double y, double width, double height){
+        this.setX(x);
+        this.setY(y);
+        this.setWidth(width);
+        this.setHeight(height);
+
+    }
+
+    public boolean isCollidingLeft(){
+
+        if(this.getX() <= 0){
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isCollidingRight(){
+
+        if(this.getX() + this.getWidth() >= MainApplication.winX){
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isCollidingBottom(){
+
+        if(this.getY() + this.getHeight() >= MainApplication.winY){
+            return true;
+        }
+
+        return false;
+    }
+
+    public void moveRight(){
+        this.setX( this.getX() + vX);
+    }
+
+    public void moveLeft(){
+        this.setX( this.getX() - vX);
+    }
+
+    public void jump(){
+        this.setTranslateY(vY);
+        vY -= g;
+        if(this.getY() > MainApplication.winY){
+            vY = 0;
+            this.setY(MainApplication.winY - this.getHeight());
+        }
+    }
+
 }
