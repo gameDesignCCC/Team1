43d50fe0439d2a6ff5327da5c7b2ee65d006e1f3
JacobDixon0 <me@jacobdixon.us>
2018-11-03 10:54:19 -0400

    Added player death mechanic.

--- Game/src/Player.java
+++ Game/src/Player.java
@@ -11,6 +11,9 @@
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import sun.applet.Main;
 
 public class Player extends ImageView {
 
@@ -31,7 +34,12 @@ public class Player extends ImageView {
     // Gravity
     static double g = 1;
 
+    private static boolean isDead = false;
+    private static int hp = 100;
+
     Collision playerCollision = new Collision();
+
+    Rectangle hpBar = new Rectangle();
 
     Player(double x, double y, double width, double height, Image sprite) {
         this.setX(x);
@@ -39,6 +47,10 @@ public class Player extends ImageView {
         this.setFitWidth(width);
         this.setFitHeight(height);
         this.setImage(sprite);
+        hpBar.setHeight(10);
+        hpBar.setWidth(width);
+        hpBar.setFill(Color.GREEN);
+        MainApplication.addToRoot(hpBar);
     }
 
     /**
@@ -104,106 +116,135 @@ public class Player extends ImageView {
      */
     public void onUpdate(boolean up, boolean left, boolean right) {
 
-        vX = 0;
+        if (!isDead) {
 
-        if(!inJumpAnimation) {
-            vY = 0;
-        }
+            vX = 0;
 
-        // Player Controls
-        //  |- L&R Controls
-        if (right &&
-                !checkStageCollisionRight() &&
-                !( this.getX() + this.getFitWidth() + playerSpeed >= MainApplication.WINDOW_SIZE_X ) &&
-                playerCollision.isCollidingRight( this ) == null ) {
-
-            vX += playerSpeed;
-            isMovingRight = true;
-
-        } else if ( isMovingRight && this.getX() + this.getFitWidth() + playerSpeed >= MainApplication.WINDOW_SIZE_X ) {
-            setX( MainApplication.WINDOW_SIZE_X - this.getFitWidth() );
-            isMovingRight = false;
-
-        }
-
-        if (left &&
-                !checkStageCollisionLeft() &&
-                !( this.getX() - playerSpeed <= 0 ) &&
-                playerCollision.isCollidingLeft(this ) == null ) {
-
-            vX += playerSpeed * -1;
-            isMovingLeft = true;
-
-        } else if ( isMovingLeft && this.getX() - playerSpeed <= 0 ) {
-            setX(0);
-            isMovingLeft = false;
-
-        }
-
-        // |- Jumping Controls
-
-        if(inJumpAnimation){
-
-            if(!(this.getY() + this.getFitHeight() >= MainApplication.WINDOW_SIZE_Y ) ) {
-                vY += g;
-            }else if( this.getY() + this.getFitHeight() >= MainApplication.WINDOW_SIZE_Y ){
+            if (!inJumpAnimation) {
                 vY = 0;
-                inJumpAnimation = false;
             }
-        }
 
-        // Jump
-        if (up && !inJumpAnimation){
-            vY += -15.0;
-            inJumpAnimation = true;
+            hpBar.setWidth(hp);
 
-        }
+            // Player Controls
+            //  |- L&R Controls
+            if (right &&
+                    !checkStageCollisionRight() &&
+                    !(this.getX() + this.getFitWidth() + playerSpeed >= MainApplication.WINDOW_SIZE_X) &&
+                    playerCollision.isCollidingRight(this) == null) {
 
-        // Ends jump animation when the player collides with stage bottom.
-        if(this.getY() + this.getFitHeight() >= MainApplication.WINDOW_SIZE_Y){
-            inJumpAnimation = false;
-            this.setY(MainApplication.WINDOW_SIZE_Y - this.getFitHeight());
-        }
+                vX += playerSpeed;
+                isMovingRight = true;
 
-        // TODO : Update player falling.
-        if( !inJumpAnimation && playerCollision.isCollidingBottom(this) != null && !checkStageCollisionBottom()){
-            inJumpAnimation = true;
+            } else if (isMovingRight && this.getX() + this.getFitWidth() + playerSpeed >= MainApplication.WINDOW_SIZE_X) {
+                setX(MainApplication.WINDOW_SIZE_X - this.getFitWidth());
+                isMovingRight = false;
 
-        }
+            }
 
-        // TODO : Fix top & bottom collisions with map objects.
+            if (left &&
+                    !checkStageCollisionLeft() &&
+                    !(this.getX() - playerSpeed <= 0) &&
+                    playerCollision.isCollidingLeft(this) == null) {
 
-        move();
+                vX += playerSpeed * -1;
+                isMovingLeft = true;
 
-        StaticRect bottom = playerCollision.isCollidingBottom(this);
-        StaticRect top = playerCollision.isCollidingTop(this);
+            } else if (isMovingLeft && this.getX() - playerSpeed <= 0) {
+                setX(0);
+                isMovingLeft = false;
 
+            }
 
-        if ( top != null && bottom != null ) {
-            double dBot = getY() + getFitHeight() - bottom.getY();
-            double dTop = top.getY() + top.getHeight() - getY();
+            // |- Jumping Controls
 
-            if ( dBot < dTop ) {
+            if (inJumpAnimation) {
+
+                if (!(this.getY() + this.getFitHeight() >= MainApplication.WINDOW_SIZE_Y)) {
+                    vY += g;
+                } else if (this.getY() + this.getFitHeight() >= MainApplication.WINDOW_SIZE_Y) {
+                    vY = 0;
+                    inJumpAnimation = false;
+                }
+            }
+
+            // Jump
+            if (up && !inJumpAnimation) {
+                vY += -15.0;
+                inJumpAnimation = true;
+
+            }
+
+            // Ends jump animation when the player collides with stage bottom.
+            if (this.getY() + this.getFitHeight() >= MainApplication.WINDOW_SIZE_Y) {
+                inJumpAnimation = false;
+                this.setY(MainApplication.WINDOW_SIZE_Y - this.getFitHeight());
+            }
+
+            // TODO : Update player falling.
+            if (!inJumpAnimation && playerCollision.isCollidingBottom(this) != null && !checkStageCollisionBottom()) {
+                inJumpAnimation = true;
+
+            }
+
+            // TODO : Fix top & bottom collisions with map objects.
+
+            move();
+
+            hpBar.setX(this.getX());
+            hpBar.setY(this.getY() - 20);
+
+            StaticRect bottom = playerCollision.isCollidingBottom(this);
+            StaticRect top = playerCollision.isCollidingTop(this);
+            StaticRect cLeft = playerCollision.isCollidingLeft(this);
+            StaticRect cRight = playerCollision.isCollidingRight(this);
+
+            if (top != null && bottom != null) {
+                double dBot = getY() + getFitHeight() - bottom.getY();
+                double dTop = top.getY() + top.getHeight() - getY();
+
+                if (dBot < dTop) {
+                    inJumpAnimation = false;
+                    vY = 0.0;
+                    setY(bottom.getY() - bottom.getHeight());
+                } else {
+                    vY = 0;
+                    setY(top.getY() + top.getHeight());
+                }
+            } else if (bottom != null) {
                 inJumpAnimation = false;
                 vY = 0.0;
-                setY( bottom.getY() - bottom.getHeight());
-                System.out.println( "TOP" );
-            } else {
+                setY(bottom.getY() - bottom.getHeight());
+                if(bottom.getType() == "spike"){
+                    hp -= 120;
+                }else if(bottom.getType() == "enemy"){
+                    hp -= 5;
+                }
+            } else if (top != null) {
                 vY = 0;
                 setY(top.getY() + top.getHeight());
-                System.out.println( "BOT" );
             }
-        } else if(bottom != null){
-            inJumpAnimation = false;
-            vY = 0.0;
-            setY( bottom.getY() - bottom.getHeight());
-            System.out.println( "TOP" );
-        } else if(top != null){
-            vY = 0;
-            setY(top.getY() + top.getHeight());
-            System.out.println( "BOT" );
+
         }
 
+        if( hp <= 0 && !isDead ){
+            die();
+        }
+
+    }
+
+    public void die(){
+        isDead = true;
+        System.out.println("player died");
+        MainApplication.rmFromRoot(this);
+        MainApplication.rmFromRoot(hpBar);
+        MainApplication.getStage().setScene(Menu.deathMenu());
+    }
+
+    public void reset(){
+        this.setX(0);
+        this.setY(MainApplication.WINDOW_SIZE_Y - this.getFitHeight());
+        this.hp = 100;
     }
 
 }
