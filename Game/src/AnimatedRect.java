ba19a30fda76820a44fc1172e612ab16ed39f5ff
Jacob Dixon <me@jacobdixon.us>
2019-03-30 04:33:06 -0400

    Player left & right walk animation setup

--- Game/src/AnimatedRect.java
+++ Game/src/AnimatedRect.java
@@ -1,6 +1,6 @@
 import javafx.scene.image.Image;
 
-public class AnimatedRect extends StaticRect implements GameObject{
+public class AnimatedRect extends StaticRect implements GameObject {
 
     private Image[] frames;
     private double frameRate;
@@ -18,19 +18,19 @@ public class AnimatedRect extends StaticRect implements GameObject{
      * @param type
      */
     public AnimatedRect(double x, double y, double width, double height, Image[] frames, Type type) {
-        this(x, y, width, height, frames, type, frames.length/60.0);
+        this(x, y, width, height, frames, type, frames.length / 60.0);
     }
 
     /**
      * constructor
      *
-     * @param x      x coordinate of rectangle
-     * @param y      y coordinate of rectangle
-     * @param width  width of rectangle
-     * @param height height of rectangle
-     * @param frames sprites of rectangle
+     * @param x         x coordinate of rectangle
+     * @param y         y coordinate of rectangle
+     * @param width     width of rectangle
+     * @param height    height of rectangle
+     * @param frames    sprites of rectangle
      * @param type
-     * @param frameRate The Frame Rate of the amimation
+     * @param frameRate The Frame Rate of the animation
      */
     public AnimatedRect(double x, double y, double width, double height, Image[] frames, Type type, double frameRate) {
         super(x, y, width, height, frames[0], type);
@@ -40,8 +40,8 @@ public class AnimatedRect extends StaticRect implements GameObject{
         this.lastTimeMS = System.currentTimeMillis();
     }
 
-    public void onUpdate( ) {
-        if ( (System.currentTimeMillis() - lastTimeMS) / 100.0 >= 1.0 / frameRate ) {
+    public void onUpdate() {
+        if ((System.currentTimeMillis() - lastTimeMS) / 100.0 >= 1.0 / frameRate) {
             currentFrame = (currentFrame + 1) % frames.length;
             this.setSprite(frames[currentFrame]);
             lastTimeMS = System.currentTimeMillis();
